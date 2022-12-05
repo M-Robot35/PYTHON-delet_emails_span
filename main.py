663d81698783bc50a7afca90f3e0befeb2a28ba4
THIAGO TELES <96987684+M-Robot35@users.noreply.github.com>
2022-12-05 15:07:22 -0300

    Update main.py

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ https://sqlitebrowser.org/dl/
 """
 
 login = 'thiago.teles725@gmail.com'  # seu email
-senha = 'ozuefpmcktttpigg'  # sua senha unica gmail
+senha = 'ozuefpmcktttp'  # sua senha unica gmail
 
 hostGmail = 'smtp.gmail.com'
 
